a5e2efa5e3af5d033293bc4826b55054739ef2a5
Павел Юров <peyurov@gmail.com>
2022-12-09 12:48:45 +0300

    UP

--- Анонимные методы/Program.cs
+++ Анонимные методы/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Анонимные_методы
 {
+    public delegate void Anonim();
+    public delegate string AnonimString(string s);
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Anonim anonim = delegate
+            {
+                Console.WriteLine("Анонимный метод");
+            };
+
+            anonim();
+
+            AnonimString anonim1 = delegate (string s)
+            {
+                Console.WriteLine(s);
+                return s;
+            };
+
+            string result = anonim1("Анонимный метод с принимаемый и возвращаемой значением");
         }
     }
 }
