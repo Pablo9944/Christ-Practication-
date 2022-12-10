0c59a6394a0f915df2454203dbe55ddd1b80e979
Павел Юров <peyurov@gmail.com>
2022-12-10 11:06:48 +0300

    up

---
+++ Nullable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> n = null;
+            int? number1 = null;
+            int? number2 = null;
+
+            int? number3 = number1 + number2 + n;
+
+            number1 = number2 ?? 10;
+            Console.WriteLine(number1);
+
+
+        }
+    }
+}
---
+++ Ограничения универсальных типов. where/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ограничения_универсальных_типов.where
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
